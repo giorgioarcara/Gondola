ed075df32e8bb205846468f46f6a810967f43110
anemos96 <ettorenapoli.en@gmail.com>
2025-09-29 11:42:29 +0200

    Fixed Code and documentation

--- functions/GT_funcs/statistic/GTvec_ttest2.m
+++ functions/GT_funcs/statistic/GTvec_ttest2.m
@@ -1,24 +1,35 @@
-%% GTvec_ttest2(GTres_diff)
-%
-% This functions perform a indepdent sample t-test.
-% Currently you can also use the function supplying a struct with difference scores
-% as those from GTdifference function.
-%
-% 
-%
-% Author: Girogio Arcara
-%
-% Data : 4/02/2018;
-%
-%
-function [tmat, pmat] = GTvec_ttest2(GTstruct1, GTstruct2, resfield);
+function [tvec, pvec] = GTvec_ttest2(GTstruct1, GTstruct2, Field);
+    arguments
+        GTstruct1 (1, :) struct
+        GTstruct2 (1, :) struct
+        Field (1, 1) string
+    end
 
 
-data1 = [GTstruct1.(resfield)];
-data2 = [GTstruct2.(resfield)];
+%% GTvec_ttest2(GTstruct1, GTstruct2, Field)
+%
+% This function performs a independent sample t-test over vectors (GT
+% measures)
+%
+% Inputs:
+%   GTstruct1 (struct): First GTstruct object 
+%   GTstruct2 (struct): Second GTstruct object
+%   Field (string): Name of the field of the GTstructs containing the
+%   vectors for which you want to calculate t-test
+%
+% Authors: Giorgio Arcara, Ettore Napoli, Alessandro Tonin
+%
+% Data : 29/09/2025;
+%
+%
 
-datamat1 = reshape(data1, length(GTstruct1(1).(resfield)), length(GTstruct1));
-datamat2 = reshape(data2, length(GTstruct2(1).(resfield)), length(GTstruct2));
+
+
+data1 = [GTstruct1.(Field)];
+data2 = [GTstruct2.(Field)];
+
+datamat1 = reshape(data1, length(GTstruct1(1).(Field)), length(GTstruct1));
+datamat2 = reshape(data2, length(GTstruct2(1).(Field)), length(GTstruct2));
 
 % suboptimal (made to work on all cells of a symmetric matrix) could be
 % optmized
@@ -31,15 +42,13 @@ datamat2 = reshape(data2, length(GTstruct2(1).(resfield)), length(GTstruct2));
 %prback = reshape(datamat, size(pr, 1), size(pr, 1), size(pr,3));
 %all(prback(:)==pr(:))
 
-pvec = zeros(1, size(datamat, 1));
+pvec = zeros(1, size(datamat1, 1));
 
-for iT = 1:size(datamat, 1)
-   [~, pvec(iT), ~, stats]=ttest2(datamat(iT,:));
+for iT = 1:size(datamat1, 1)
+   [~, pvec(iT), ~, stats]=ttest2(datamat1(iT,:), datamat2(iT, :));
    tvec(iT) = stats.tstat;
 end;
 
 
-tmat = tvec;
-pmat = pvec;
 
 
